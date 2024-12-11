c37b13b60ab5f08fc3fa7f5299147654abf8bb71
LeandroReichardt <163359722+LeandroReichardt@users.noreply.github.com>
2024-12-11 00:01:21 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ConversordeMoedas from './components/ConversordeMoedas'
+
+function App() {
+
+  return (
+    <>
+    <ConversordeMoedas/>
+      </>
+  )
+}
+
+export default App
